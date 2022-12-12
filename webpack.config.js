3f1d0142afd11b838a87ceb4e5ba4cb8ced64609
Dimitar Petrov <dimitarpetrov.02.98@gmail.com>
2022-12-12 14:52:15 +0200

    feat: add eslint webpack plugin to show errors when compiled

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,31 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+/** @format */
+
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
 
 module.exports = {
   output: {
-    path: path.join(__dirname, "/dist"), // the bundle output path
-    filename: "bundle.js", // the name of the bundle
+    path: path.join(__dirname, '/dist'), // the bundle output path
+    filename: 'bundle.js' // the name of the bundle
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html", // to import index.html file inside index.js
+    new ESLintPlugin({
+      files: 'src/**/*.(js|jsx|ts|tsx)',
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      overrideConfigFile: '.eslintrc.json',
+      lintDirtyModulesOnly: true,
+      emitError: true,
+      emitWarning: true,
+      failOnError: false,
+      failOnWarning: false
     }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html' // to import index.html file inside index.js
+    })
   ],
   devServer: {
-    port: 3030, // you can change the port
+    port: 3030 // you can change the port
   },
   module: {
     rules: [
@@ -20,18 +33,18 @@ module.exports = {
         test: /\.(js|jsx)$/, // .js and .jsx files
         exclude: /node_modules/, // excluding the node_modules folder
         use: {
-          loader: "babel-loader",
-        },
+          loader: 'babel-loader'
+        }
       },
       {
         test: /\.(sa|sc|c)ss$/, // styles files
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader']
       },
       {
         test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
-        loader: "url-loader",
-        options: { limit: false },
-      },
-    ],
-  },
-};
+        loader: 'url-loader',
+        options: { limit: false }
+      }
+    ]
+  }
+}
