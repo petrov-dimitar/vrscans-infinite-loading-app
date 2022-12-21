435d6e3959ad2bd5b31fcf084a7ef0fb9ed56418
dimitar.petrov <dimitarpetrov.02.98@gmail.com>
2022-12-21 18:55:03 +0200

    feat: add express, mongodb, mongoose and User Model

---
+++ back-end/react-api-chaosgroup/app.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const User = require("./models/UserModel");
+const bodyParser = require('body-parser')
+
+const app = express();
+
+dotenv.config({ path: "./.env" });
+const jsonParser = bodyParser.json();
+
+console.log(dotenv.config);
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(DB).then((con) => {
+  console.log(con.connections);
+  console.log("success");
+});
+
+app.post("/signup", jsonParser, async (req, res) => {
+  console.log("body", req.body);
+  const newUser = await User.create(req.body);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: newUser,
+    },
+  });
+});
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello from server", app: "name" });
+});
+
+app.post("/", (req, res) => {
+  res.send("You can post to this endpoint...");
+});
+
+const port = process.env.PORT || 1337;
+
+// function handler(req, res) {
+//      if(req.url === "/"){
+//         res.writeHead(200, {'Content-type':'text/plain'});
+//          res.write('Hello, I am a webserver !');
+//          res.end();
+//     }
+//     else if(req.url === "/users"){
+//         const users = data.users
+//         res.end(JSON.stringify(users))
+//     }
+//     else if(req.url === "/materials"){
+//         const materials = data.materials
+//         res.end(JSON.stringify(materials))
+//     }
+//     else if(req.url === "/vrscans"){
+//         const vrscans = data.vrscans
+//         res.end(JSON.stringify(vrscans))
+//     }
+//     else if(req.url === "/tags"){
+//         const tags = data.tags
+//         res.end(JSON.stringify(tags))
+//     }
+//     else if(req.url === "/colors"){
+//         const colors = data.colors
+//         res.end(JSON.stringify(colors))
+//     }
+//     else if(req.url === "/industries"){
+//         const industries = data.industries
+//         res.end(JSON.stringify(industries))
+//     }
+//     else if(req.url === "/manufacturers"){
+//         const manufacturers = data.manufacturers
+//         res.end(JSON.stringify(manufacturers))
+//     }
+//     else if(req.url === "/favorites"){
+//         res.writeHead(200, {'Content-type':'text/plain'});
+//         res.write('Sorry, no such path exists YET');
+//         res.end();
+//     }
+//     // TODO: Add favorites
+//     else{
+//         res.writeHead(200, {'Content-type':'text/plain'});
+//         res.write('Sorry, no such path exists');
+//         res.end();
+//     }
+// }
+
+app.listen(port);
---
+++ back-end/react-api-chaosgroup/models/UserModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "please provide email"],
+    lowercase: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "please provide password"],
+  },
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
