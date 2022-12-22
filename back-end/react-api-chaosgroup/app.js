dd91029747f6fb1c5713bcbbc1a573bbf6cbc6ad
dimitar.petrov <dimitarpetrov.02.98@gmail.com>
2022-12-22 10:01:58 +0200

    feat(BE): add password encryption

--- back-end/react-api-chaosgroup/app.js
+++ back-end/react-api-chaosgroup/app.js
@@ -4,10 +4,13 @@ const mongoose = require("mongoose");
 const User = require("./models/UserModel");
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
+const catchAsync = require("./utils/catchAsync");
+const AppError = require("./utils/appError");
+const data = require("./data.json");
 
 const app = express();
 
-dotenv.config({ path: "./.env" });
+dotenv.config({ path: "../../.env" });
 const jsonParser = bodyParser.json();
 
 console.log(dotenv.config);
@@ -24,24 +27,28 @@ mongoose.connect(DB).then((con) => {
   console.log("success");
 });
 
-app.post("/signup", jsonParser, async (req, res) => {
-  console.log("body", req.body);
-  const newUser = await User.create(req.body);
+app.post(
+  "/signup",
+  jsonParser,
+  catchAsync(async (req, res, next) => {
+    console.log("body", req.body);
+    const newUser = await User.create(req.body);
 
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
 
-  res.status(200).json({
-    status: "success",
-    token,
-    data: {
-      user: newUser,
-    },
-  });
-});
+    res.status(200).json({
+      status: "success",
+      token,
+      data: {
+        user: newUser,
+      },
+    });
+  })
+);
 
-app.post("/login",jsonParser, async (req, res) => {
+app.post("/login", jsonParser, async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
@@ -51,6 +58,10 @@ app.post("/login",jsonParser, async (req, res) => {
   }
 
   const user = await User.findOne({ email, password }).select("+password");
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError("Incorrect email or password", 401));
+  }
 
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
@@ -71,53 +82,53 @@ app.post("/", (req, res) => {
   res.send("You can post to this endpoint...");
 });
 
-const port = process.env.PORT || 1337;
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello from server", app: "name" });
+});
 
-// function handler(req, res) {
-//      if(req.url === "/"){
-//         res.writeHead(200, {'Content-type':'text/plain'});
-//          res.write('Hello, I am a webserver !');
-//          res.end();
-//     }
-//     else if(req.url === "/users"){
-//         const users = data.users
-//         res.end(JSON.stringify(users))
-//     }
-//     else if(req.url === "/materials"){
-//         const materials = data.materials
-//         res.end(JSON.stringify(materials))
-//     }
-//     else if(req.url === "/vrscans"){
-//         const vrscans = data.vrscans
-//         res.end(JSON.stringify(vrscans))
-//     }
-//     else if(req.url === "/tags"){
-//         const tags = data.tags
-//         res.end(JSON.stringify(tags))
-//     }
-//     else if(req.url === "/colors"){
-//         const colors = data.colors
-//         res.end(JSON.stringify(colors))
-//     }
-//     else if(req.url === "/industries"){
-//         const industries = data.industries
-//         res.end(JSON.stringify(industries))
-//     }
-//     else if(req.url === "/manufacturers"){
-//         const manufacturers = data.manufacturers
-//         res.end(JSON.stringify(manufacturers))
-//     }
-//     else if(req.url === "/favorites"){
-//         res.writeHead(200, {'Content-type':'text/plain'});
-//         res.write('Sorry, no such path exists YET');
-//         res.end();
-//     }
-//     // TODO: Add favorites
-//     else{
-//         res.writeHead(200, {'Content-type':'text/plain'});
-//         res.write('Sorry, no such path exists');
-//         res.end();
-//     }
-// }
+app.get("/users", (req, res) => {
+  const users = data.users;
+  res.status(200).json(users);
+});
+
+app.get("/materials", (req, res) => {
+  const materials = data.materials;
+  res.status(200).json(materials);
+});
+
+app.get("/vrscans", (req, res) => {
+  const vrscans = data.vrscans;
+  res.status(200).json(vrscans);
+});
+
+app.get("/tags", (req, res) => {
+  const tags = data.tags;
+  res.status(200).json(tags);
+});
+
+app.get("/colors", (req, res) => {
+  const colors = data.colors;
+  res.status(200).json(colors);
+});
+
+app.get("/industries", (req, res) => {
+  const colors = data.industries;
+  res.status(200).json(industries);
+});
+
+app.get("/manufacturers", (req, res) => {
+  const colors = data.manufacturers;
+  res.status(200).json(manufacturers);
+});
+
+// Favourites
+// TODO: Make protected route
+// TODO Add resource to user collection
+app.get("/favorites", (req, res) => {
+  res.status(400);
+});
+
+// Start server
+const port = process.env.PORT || 1337;
 
 app.listen(port);
--- back-end/react-api-chaosgroup/models/UserModel.js
+++ back-end/react-api-chaosgroup/models/UserModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -13,6 +14,24 @@ const userSchema = new mongoose.Schema({
     select: false,
   },
 });
+
+userSchema.pre("save", async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified("password")) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  next();
+});
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model("User", userSchema);
 
