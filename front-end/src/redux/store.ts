3e8f661593b519e1962de65a0432773b3836fed3
dimitar.petrov <dimitarpetrov.02.98@gmail.com>
2022-12-29 21:33:37 +0200

    feat(fe): add redux service for vrscans fetch

--- front-end/src/redux/store.ts
+++ front-end/src/redux/store.ts
@@ -1,13 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit'
-import filtersReducer from 'modules/ExplorePage/redux/slice'
+import { configureStore } from '@reduxjs/toolkit';
+import { vrScansApi } from 'modules/ExplorePage/redux/service';
+import filtersReducer from 'modules/ExplorePage/redux/slice';
 
 export const store = configureStore({
   reducer: {
-    filters: filtersReducer,
+    // Add the generated reducer as a specific top-level slice
+    [vrScansApi.reducerPath]: vrScansApi.reducer,
+    filters: filtersReducer
   },
-})
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(vrScansApi.middleware)
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
