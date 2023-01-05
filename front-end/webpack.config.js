653ad238c5f36eaea5c29888cafc68dcf8405173
dimitar.petrov <dimitarpetrov.02.98@gmail.com>
2023-01-05 23:37:51 +0200

    fix(fe): code refactoring

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -3,10 +3,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
-const dotenv = require('dotenv');
 const webpack = require('webpack');
-
-// import copy plugin
 
 module.exports = (env) => {
   const dotenv = require('dotenv').config({ path: '../' + '.env' }).parsed;
