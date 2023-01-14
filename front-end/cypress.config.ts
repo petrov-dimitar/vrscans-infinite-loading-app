02d3e16d9811360c27401a0d00e20cf18f102cdb
dimitar.petrov <dimitarpetrov.02.98@gmail.com>
2023-01-14 22:05:02 +0200

    fix(fe): add cypress ci/cd

--- front-end/cypress.config.ts
+++ front-end/cypress.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'cypress';
 
+require('dotenv').config({ path: '../' + '.env' });
+
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      config.env = {
+        ...process.env,
+        ...config.env
+      };
+      return config;
     }
-  },
-  env: {
-    REACT_APP_BASE_URL: 'http://localhost:3030'
   },
   component: {
     devServer: {
